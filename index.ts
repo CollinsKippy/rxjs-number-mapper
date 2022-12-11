f96be58011bb9a0702e3a82bdbc50462ef7877c9
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2022-12-11 18:24:06 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ of(10, 50, 100, 150)
 
 console.log('-------------------custom number mapper--------------');
 
-const myNumberMapper = (transformationFn: (val: number) => number) => {
+const myNumberMapper = (transformationFn: (val: number) => number | Error) => {
   return (input: Observable<number>) => {
     return new Observable((observer) => {
       return input.subscribe({
@@ -66,15 +66,13 @@ of(1, 2, 3, 4)
   .pipe(
     myNumberMapper((x: number) => {
       if (x === 3) {
-        throwError(() => new Error(`Errored ${x} ${Date.now()}`));
+        return new Error(`Errored ${x} ${Date.now()}`);
       } else {
         return x + 2;
       }
     }),
     catchError((err) => {
-      console.warn(`CatchError: ${err}`);
-      errorDiv.textContent = err;
-      return throwError(() => `Errored ${err} ${Date.now()}`);
+      return of(new Error(`Rethrown ${err} ${Date.now()}`));
     })
   )
   .subscribe({
@@ -86,9 +84,6 @@ of(1, 2, 3, 4)
     },
     error: (myErr) => {
       console.error('Came through: ', myErr);
-      const div: HTMLElement = document.createElement('div');
-      div.append(`${myErr}`);
-      errorDiv.appendChild(div);
     },
     complete: console.log,
   });
